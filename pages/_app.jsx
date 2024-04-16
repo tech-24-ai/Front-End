826747e03ae5db0d685072d56c761632fbfe7c35
Vandan <vandan.javiya@intelloger.com>
2024-04-16 19:40:42 +0530

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -78,6 +78,7 @@ class MyApp extends App {
             href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
             rel="stylesheet"
           />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet"></link>
           </Head>
           <Timer />
           <Cookies />
