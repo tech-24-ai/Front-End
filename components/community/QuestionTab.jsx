f50683174ff7df7d53b37e3d571eaa2a3a0a7932
shivam8447 <Shivam.Mourya@intelloger.com>
2024-05-03 19:49:16 +0530

    isSearch and askQuestion issue

--- components/community/QuestionTab.jsx
+++ components/community/QuestionTab.jsx
@@ -61,7 +61,13 @@ const SubmitButton = ({ form, children }) => {
   );
 };
 
-const QuestionTab = ({ getAllCrud, showAlert, success }) => {
+const QuestionTab = ({
+  getAllCrud,
+  showAlert,
+  success,
+  isSearch,
+  askQuestion,
+}) => {
   const router = useRouter();
   const { community } = router.query;
   const [description, setDescription] = useState("");
