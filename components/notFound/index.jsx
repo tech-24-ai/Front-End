c2b0e785b58fe5b4c9a96bb38105ce1b62d94b06
shubhamparteti <98079050+shubhamparteti@users.noreply.github.com>
2024-05-23 14:04:50 +0530

    Update index.jsx

--- components/notFound/index.jsx
+++ components/notFound/index.jsx
@@ -6,7 +6,7 @@ const NotFound = ({ children }) => {
     <div className="no-result-found-section" style={{ margin: "0 auto" }}>
       <NotFoundIcon />
       <h5 className="title">No result found</h5>
-      <p className="message">We can't find any item matching your search</p>
+      {/* <p className="message">We can't find any item matching your search</p> */}
       {children}
     </div>
   );
